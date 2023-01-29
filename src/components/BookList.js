6d80c38c18970bb6e6c34aba6d7b42504391f750
Brandon Alexander <bjvonalexander@gmail.com>
2023-01-29 08:19:51 -0600

    Section 6: How to Handle Forms (105 - 107)

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,5 +1,10 @@
-function BookList() {
-    return <div>Book List</div>;
+import BookShow from "./BookShow";
+
+function BookList({ books}) {
+    const renderBooks = books.map((book) => {
+        return <BookShow key={book.id} book={book} />;
+    });
+    return <div className="book-list">{renderBooks}</div>;
 }
 
 export default BookList;
--- src/components/BookShow.js
+++ src/components/BookShow.js
@@ -1,5 +1,6 @@
-function BookShow() {
-    return <div>Book Show</div>;
+function BookShow({ book }) {
+
+    return <div className="book-show">{book.title}</div>;
 }
 
 export default BookShow;
