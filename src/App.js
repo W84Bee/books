936d9d5c4352cf989ff63c36a315c783685481c5
Brandon Alexander <bjvonalexander@gmail.com>
2023-01-29 06:17:15 -0600

    Section 6: How to Handle Forms (85 - 88)
    85. State Location
    86. Reminder on Event Handler
    87. Extra CSS
    88. Receiving New Titles

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
-
+import { useState } from "react";
+import BookCreate from "./components/BookCreate";
 
 function App() {
-    return <div>Books Application</div>;
+    const [books, setBooks] = useState([]);
+
+    const createBook = (title) => {
+        console.log('Need to add book with:', title);
+    };
+
+
+    return <div><BookCreate onCreate={createBook} /></div>;
 }
 
 export default App;
