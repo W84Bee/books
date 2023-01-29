c1dad4f8771756d56bf1b4ca958dbc504aacd4cf
Brandon Alexander <bjvonalexander@gmail.com>
2023-01-29 09:43:15 -0600

    Section 6: Closing the Form on Submit

--- src/components/BookShow.js
+++ src/components/BookShow.js
@@ -1,13 +1,33 @@
-function BookShow({ book, onDelete }) {
-    const handleClick = () => {
+import { useState } from "react";
+import BookEdit from "./BookEdit";
+
+function BookShow({ book, onDelete, onEdit }) {
+    const [showEdit, setShowEdit] = useState(false);
+
+    const handleDeleteClick = () => {
         onDelete(book.id);
     };
 
+    const handleEditClick = () => {
+        setShowEdit(!showEdit);
+    };
+
+    const handleSubmit = () => {
+        setShowEdit(false);
+    };
+
+    let content = <h3>{book.title}</h3>;
+   
+    if (showEdit) {
+        content = <BookEdit onSubmit={handleSubmit} onEdit={onEdit} book={book} />;
+    }
+
     return (
         <div className="book-show">
-            {book.title}
+            {content}
             <div className="actions">
-                <button className="delete" onClick={handleClick}>Delete</button>
+                <button className="edit" onClick={handleEditClick}></button>
+                <button className="delete" onClick={handleDeleteClick}>Delete</button>
             </div>
         </div>
     );
