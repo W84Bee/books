56f4745aba96156737cb5558aa76fa8ecb758679
Brandon Alexander <bjvonalexander@gmail.com>
2023-02-02 18:30:36 -0600

    Introduction To Context.Provider
    - keywords {createContext, BooksContext.Provider}.
    - This method is used to Share Values throughout the components even if they are not directly connected.

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,10 +1,18 @@
 import BookShow from "./BookShow";
+import { useContext } from "react";
+import BooksContext from "../context/books";
 
 function BookList({ books, onDelete, onEdit}) {
+    const { count, incrementCount } = useContext(BooksContext);
+
     const renderBooks = books.map((book) => {
         return <BookShow key={book.id} book={book} onDelete={onDelete} onEdit={onEdit} />;
     });
-    return <div className="book-list">{renderBooks}</div>;
+    return <div className="book-list">
+        {count}
+        <button onClick={incrementCount}>Create!</button>
+        {renderBooks}
+    </div>;
 }
 
 export default BookList;
